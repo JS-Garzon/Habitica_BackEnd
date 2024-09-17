9b90256a588dacef500f31e33ebc7fcde4b7821c
sebastian garzon <juansebastiangarzonchamorro@gmail.com>
2024-09-17 13:51:06 -0500

    Modules was imported

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
+import { DatabaseModule } from './database/database.module';
+import { HabitsModule } from './modules/habits/habits.module';
+import { UsersModule } from './modules/users/users.module';
 import { AppService } from './app.service';
+import { HabitHistoryModule } from './modules/habit-history/habit-history.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { GoalModule } from './modules/goal/goal.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    UsersModule,
+    HabitsModule,
+    HabitHistoryModule,
+    AuthModule,
+    GoalModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
