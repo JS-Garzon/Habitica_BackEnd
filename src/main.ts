fb4a38f5d7c25a4204b6a9e90993ff0e13e02058
sebastian garzon <juansebastiangarzonchamorro@gmail.com>
2024-10-24 00:04:38 -0500

    New front url was added

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: ['http://localhost:4200', 'https://habitica-front-end.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  //new port
-  console.log('DB_HOST:', process.env.DATABASE_HOST); // Reemplaza 'DB_HOST' con el nombre de tu variable
-  console.log('DB_PORT:', process.env.DATABASE_PORT); // db
-  const port = process.env.PORT || 8080;
-  await app.listen(port);
+  // const port = process.env.DATABASE_PORT || 8080;
+  await app.listen(3000);
 }
 bootstrap();
